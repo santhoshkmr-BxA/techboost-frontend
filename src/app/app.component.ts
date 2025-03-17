f85d7729f662e214d86490ed8cf9e3083c68db5e
Santhosh KMR <Santhosh.K@bounteous.com>
2025-03-17 20:51:58 +0530

    Added Tailwind CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 
+import { CardModule } from 'primeng/card';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  standalone: true,
 })
 export class AppComponent {
   title = 'techboost-frontend';
